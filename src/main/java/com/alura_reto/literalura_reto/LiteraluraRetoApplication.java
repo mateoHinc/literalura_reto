15c21443dcf9a4f55b3e71c6ceb1a410aec33991
mateoHinc <mateohincapie219600@correo.itm.edu.co>
2024-05-15 17:59:07 -0500

    Agregar archivos

---
+++ src/main/java/com/alura_reto/literalura_reto/LiteraluraRetoApplication.java
@@ -0,0 +1,13 @@
+package com.alura_reto.literalura_reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraRetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraRetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura_reto/literalura_reto/LiteraluraRetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura_reto.literalura_reto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraRetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
